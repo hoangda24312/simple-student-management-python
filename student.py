ca5e85c7974269723572a7f5349e512ecd821792
hoangda24312 <thanosligma942@gmail.com>
2025-10-02 22:32:16 +0700

    them hien thi danh sach

--- student.py
+++ student.py
@@ -7,5 +7,20 @@ class Student:
         self.nam = nam
         self.lop = lop
     
+    @staticmethod
+    def hienThiDanhSachSinhVien(db):
+        query = "Select*from student"
+        danh_sach = db.chay_lenh(query)
+        danh_sach_tam = []
+        if danh_sach:
+            for val in danh_sach:
+                ten,id,ngay,thang,nam,lop = val
+                hoc_sinh = Student(ten,id,ngay,thang,nam,lop)
+                danh_sach_tam.append(hoc_sinh)
+            return danh_sach
+        else:
+            return []
+
+
 
     
