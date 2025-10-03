a3afce37fa7a967cc742fa21995d70bad9743fb1
hoangda24312 <thanosligma942@gmail.com>
2025-10-03 20:52:22 +0700

    tim sinh vien theo ten

--- student.py
+++ student.py
@@ -7,14 +7,30 @@ class Student:
         self.nam = nam
         self.lop = lop
     
+
     @staticmethod
-    def hienThiDanhSachSinhVien(db):
-        query = "Select*from student"
-        danh_sach = db.chay_lenh(query)
+    def timSinhVien(db, id):
+        query = "Select*from student where id=%s"
+        danh_sach = db.select(query,(id),)
         danh_sach_tam = []
         if danh_sach:
             for val in danh_sach:
-                ten,id,ngay,thang,nam,lop = val
+                id,ten,ngay,thang,nam,lop = val
+                hoc_sinh = Student(id,ten,ngay,thang,nam,lop)
+                danh_sach_tam.append(hoc_sinh)
+            return danh_sach_tam
+        else:
+            return []
+
+    
+    @staticmethod
+    def hienThiDanhSachSinhVien(db):
+        query = "Select*from student"
+        danh_sach = db.select(query)
+        danh_sach_tam = []
+        if danh_sach:
+            for val in danh_sach:
+                id,ten,ngay,thang,nam,lop = val
                 hoc_sinh = Student(ten,id,ngay,thang,nam,lop)
                 danh_sach_tam.append(hoc_sinh)
             return danh_sach
