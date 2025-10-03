23c94cb7789f242dfdb76b4b1d9ccb47a82c6370
hoangda24312 <thanosligma942@gmail.com>
2025-10-03 20:58:46 +0700

    them va xoa sinh vien

--- student.py
+++ student.py
@@ -7,11 +7,21 @@ class Student:
         self.nam = nam
         self.lop = lop
     
+    def themSinhVien(self,db):
+        query = "Insert into student (id,ten,ngay,thang,nam,lop) values (%s,%s,%s,%s,%s,%s)"
+        params = (self.id,self.ten,self.ngay,self.thang,self.nam,self.lop)
+        db.chay_lenh(query,params)
+    
+    def xoaSinhVien(self,db):
+        query = "Delete*from student where id=%s"
+        db.chay_lenh(query,(self.id,))
+
+    
 
     @staticmethod
     def timSinhVien(db, id):
         query = "Select*from student where id=%s"
-        danh_sach = db.select(query,(id),)
+        danh_sach = db.select(query,(id,))
         danh_sach_tam = []
         if danh_sach:
             for val in danh_sach:
